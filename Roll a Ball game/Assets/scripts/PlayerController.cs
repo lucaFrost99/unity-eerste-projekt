6ab3b4dbf0a550ac891eae1e8ff0b2ba0ed5912c
lucaFrost99 <137787353+lucaFrost99@users.noreply.github.com>
2024-03-21 15:51:15 +0100

    Displaying Score and Text

--- Roll a Ball game/Assets/scripts/PlayerController.cs
+++ Roll a Ball game/Assets/scripts/PlayerController.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-    
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -16,6 +19,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+
+        setCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,6 +32,15 @@ public class PlayerController : MonoBehaviour
 
        movementX = movementVector.x;
        movementY = movementVector.y;
+    }
+
+    void setCountText()
+    {
+        countText.text ="Count: " + count.ToString();
+        if (count >= 12)
+            {
+                winTextObject.SetActive(true);
+            }
     }
 
     void FixedUpdate()
@@ -37,6 +54,11 @@ public class PlayerController : MonoBehaviour
     if (other.gameObject.CompareTag("PickUp")) 
        {
         other.gameObject.SetActive(false);
+        count = count + 1;
+
+        setCountText();
+
+
        }
    }
 }
